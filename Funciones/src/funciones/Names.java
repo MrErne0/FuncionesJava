a56eb6b8fd79724f7931f949ce1312b014cf6228
MrErne0 <96149962+MrErne0@users.noreply.github.com>
2022-02-07 19:40:42 -0600

    Add files via upload
    Ejercicio equipo

---
+++ Funciones/src/funciones/Names.java
@@ -0,0 +1,31 @@
+package funciones;
+import java.util.Scanner;
+
+public class Names {
+
+public static void main(String[] args) {
+	Scanner console = new Scanner( System.in );
+    System.out.println( "Enter your name:  " );
+    String name = console.next();
+    printNameCharacters( name );
+    printNameLength( name );
+    console.close();
+
+		
+	}
+private static void printNameLength( String name )
+{
+  int s= name.length();
+  System.out.println("La longitud de tu nombre es de " + s);
+  
+}
+
+private static void printNameCharacters( String name )
+{
+	for(int i=0;i<name.length();i++) {
+		char letter= name.charAt(i);
+		System.out.println(letter);
+	}
+}
+	}
+
